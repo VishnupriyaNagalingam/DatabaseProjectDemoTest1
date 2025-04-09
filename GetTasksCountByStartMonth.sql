ef0766f2ae914d2c2e82ae87146279e84c1bb0d9
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-09 22:34:42 +0530

    Create New Proc: GetTasksCountByStartMonth

---
+++ GetTasksCountByStartMonth.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE [dbo].[GetTasksCountByStartMonth]
+  @Startdate date,
+  @Enddate date
+AS
+  SELECT convert(VARCHAR(7),TaskStartDate, 120) as TaskStartMonth, Count(*) as TaskCount
+  from Tasks
+  GROUP BY convert(VARCHAR(7),TaskStartDate, 120)
+RETURN 0

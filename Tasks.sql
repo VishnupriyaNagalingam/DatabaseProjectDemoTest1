115a2c3e2bd72d40877787cf1d5422d6d90f45c6
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-09 22:36:40 +0530

    Add New Column in Tasks: TaskOwner

--- Tasks.sql
+++ Tasks.sql
@@ -3,5 +3,6 @@ CREATE TABLE [dbo].[Tasks]
   [TaskId] INT NOT NULL PRIMARY KEY,
   [Sourcekey] INT,
   TaskStartDate Date,
-  TaskCompleteDate Date
+  TaskCompleteDate Date,
+  TaskOwner VARCHAR(100)
 )

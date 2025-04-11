939099498f83fc482e7f0dbbe871ebf760b61a95
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-11 16:05:37 +0530

    changes

--- Stored Procedures/GetTasksCountByStartMonth.sql
+++ Stored Procedures/GetTasksCountByStartMonth.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE [dbo].[GetTasksCountByStartMonth]
   @Startdate date,
   @Enddate date
 AS
-  SELECT convert(VARCHAR(7),TaskStartDate, 120) as TaskStartMonth, Count(*) as TaskCount
+  SELECT convert(VARCHAR(7),TaskStartDate, 120) as TaskStartMonth, TaskSource, Count(*) as TaskCount
   from Tasks
-  GROUP BY convert(VARCHAR(7),TaskStartDate, 120)
+  GROUP BY convert(VARCHAR(7),TaskStartDate, 120),TaskSource
 RETURN 0
--- Update TaskSource.PostDeployment1.sql
+++
@@ -1,6 +0,0 @@
--- This file contains SQL statements that will be executed after the build script.
-INSERT INTO dbo.Tasks (TaskId, Sourcekey, TaskStartDate, TaskCompleteDate, TaskOwner, TaskManager)
-VALUES 
-(10, 10010, '2024-08-03', '2024-08-05', 'Alice', 'John')
-
-Update Tasks set TaskSource=Case when TaskStatus='Completed' then 'BPM' else 'Flightdeck Task' END

4f046deaa7c921c204180ccc2f0a790467eb23f6
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-11 16:12:01 +0530

    test

--- obj/Debug/postdeploy.sql
+++ obj/Debug/postdeploy.sql
@@ -1,3 +1,3 @@
 ﻿-- This file contains SQL statements that will be executed after the build script.
-Update Tasks set TaskName= case when TaskSource='BPM' then 'ASR' else 'Asrs' end
+Update Tasks set TaskName= case when TaskSource='BPM' then 'ASR' else 'sss' end
 GO

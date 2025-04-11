cb7a09b3e0c2ee5679c35237f8e52b6bc6f99c4d
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-11 17:12:57 +0530

    test

--- Script.PostDeployment1.sql
+++
@@ -1,3 +0,0 @@
--- This file contains SQL statements that will be executed after the build script.
--- This file contains SQL statements that will be executed after the build script.
-update tasks set TaskSource='BPMM'
--- obj/Debug/postdeploy.sql
+++ obj/Debug/postdeploy.sql
@@ -1,4 +1,4 @@
 ﻿-- This file contains SQL statements that will be executed after the build script.
 -- This file contains SQL statements that will be executed after the build script.
-update tasks set TaskSource='Flightdeck'
+update tasks set TaskSource='BPMM'
 GO

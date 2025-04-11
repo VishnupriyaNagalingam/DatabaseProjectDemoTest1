9c1f0a305435e51c4dfd5be685e0c2e1b07744f8
Vishnupriya Nagalingam <vishnupriyan@geakminds.com>
2025-04-11 16:07:48 +0530

    changes

--- obj/Debug/postdeploy.sql
+++ obj/Debug/postdeploy.sql
@@ -1,7 +1,3 @@
 ﻿-- This file contains SQL statements that will be executed after the build script.
-INSERT INTO dbo.Tasks (TaskId, Sourcekey, TaskStartDate, TaskCompleteDate, TaskOwner, TaskManager)
-VALUES 
-(10, 1000, '2024-08-03', '2024-08-05', 'Alice', 'John')
-
-Update Tasks set TaskSource=Case when TaskStatus='Completed' then 'BPM' else 'Flightdeck Task' END
+Update Tasks set TaskName= case when TaskSource='BPM' then 'ASR' else 'Asrs' end
 GO
--- update TaskName.PostDeployment1.sql
+++ update TaskName.PostDeployment1.sql
@@ -1,2 +1,2 @@
 -- This file contains SQL statements that will be executed after the build script.
-Update Tasks set TaskName= case when TaskSource='BPM' then 'ASR' else 'Asrs' end
+Update Tasks set TaskName= case when TaskSource='BPM' then 'ASR' else 'sss' end
